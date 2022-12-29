5bc7f96497281edc4d908aced6951f1db88e0174
ostapvasilenko <ostapvasilenko55@gmail.com>
2022-12-29 12:56:38 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,31 @@
+let slider = $(document).ready(function () {
+  $('.gallery__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    infinite: true,
+    responsive: [
+      {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      }
+    }, 
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+      }
+    }, {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+    ]
+  });
+});
 
 $(document).ready(function() {
   $('.menu__icon').click(function(){
